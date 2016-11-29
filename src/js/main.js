ae3be533d1f6d650d8bb689b3c14798605c2065f
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-11-29 23:48:05 +0200

    New main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,113 +1,132 @@
-window.onLoad = init();
-function init(){
-    c = document.getElementById("taulu");
-    ctx = c.getContext("2d");
-    window_Height = window.innerHeight;
-    window_Width = window.innerWidth;
-    ctx.canvas.width  = window_Width;
-    ctx.canvas.height = window_Height;
-    kuvat = [];
-    dx = 2;
-    dy = 2;
-
-    // Kuva classi
-    function Kuva (x, y, url) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.url = url;
-
-        nega = Math.floor((Math.random() * 2) + 1);
-        nega2 = Math.floor(Math.random() * 2);
-        koko = Math.floor((Math.random() * 2) + 1);
-        koko2 = Math.floor((Math.random() * 2) + 1);
-
-        if(nega = 1) {
-            this.dx = -koko;
-        } else {
-            this.dx = koko;
-        }
-
-        if(nega2 = 1) {
-            this.dy = -koko2;
-        } else {
-            this.dy = koko2;
-        }
-
-        this.piirra = function() {
-            var imageWidth = this.kuva.width;
-            var imageHeight = this.kuva.height;
-            var spaceX = window_Width - imageWidth;
-            var spaceY = window_Height - imageHeight;
-            var randNro = Math.floor((Math.random() * 5) + 1);
-            this.x += this.dx;
-            this.y += this.dy;
-
-            if(this.x > spaceX) {
-                this.x -= 2 * randNro;
-                this.dx = -2;
-            } else if(this.y > spaceY) {
-                this.y -= 2 * randNro
-                this.dy = -2;
-            } else if(this.x < 0 ) {
-                this.dx = 2;
-            } else if(this.y < 0) {
-                this.dy = 2;
-            }
-
-            ctx.drawImage(this.kuva, this.x, this.y);
-
-        }
-    }
-
-    olut1 = new Kuva(1, 1, 'olut.png');
-    olut2 = new Kuva(200, 200, 'olut.png');
-    olut3 = new Kuva(700, 700, 'olut.png');
-    olut4 = new Kuva(800, 70, 'olut.png');
-    olut5 = new Kuva(90, 900, 'olut.png');
-    viina1 = new Kuva(300, 300, 'saarenmaa.png');
-    viina2 = new Kuva(30, 30, 'jager.png');
-    viina3 = new Kuva(500, 500, 'jallu.png');
-    viina4 = new Kuva(900, 600, 'minttu.png');
-    kuvat = [olut1, olut2, olut3,olut4, olut5, viina1, viina2, viina3, viina4];
-
-    lataaKuvat(function() {
-        setInterval(piirraKuvat, 5);
-    });
-
-}
-
-function lataaKuvat(callback) {
-    var kuvaLkm = kuvat.length;
-    var ladattuNro = 0;
-
-    for(var o = 0; o < kuvaLkm; o++) {
-        kuvat[o].kuva = new Image();
-        kuvat[o].kuva.src = kuvat[o].url;
-        kuvat[o].kuva.onload = function() {
-            ladattuNro++;
-         };
-        if(++ladattuNro >= kuvaLkm) {
-            callback();
-        }
-    }
-}
-function piirraKuvat() {
-    var kesto = kuvat.length;
-    clear();
-    for(var i = 0; i < kesto; i++) {
-        kuvat[i].piirra();
-    }
-}
-
-function clear() {
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, c.width, c.height);
-}
-$("#taulu").click(function() {
-var video = $("#bgvid").get(0);
-var myaudio = $("#myaudio").get(0);
-video.play();
-myaudio.play();
+$(window).on('load', ()=>{
+  $("#loader").fadeOut("slow");
+  $("#music").trigger("play");
+  const video = document.getElementById("bgvid");
+  $("#bgvid").on("click", ()=> {video.play()})
 });
+
+const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {transparent: true, autoResize: true, resolution: window.devicePixelRatio});
+document.body.appendChild(renderer.view);
+// Put the renderer on screen in the corner
+renderer.view.style.position = "absolute";
+renderer.view.style.top = "0px";
+renderer.view.style.left = "0px";
+
+// Create stage
+var stage = new PIXI.Container();
+
+
+// Create textures
+const beerTexture = PIXI.Texture.fromImage('./assets/images/sprites/beer.png');
+const tartTexture = PIXI.Texture.fromImage('./assets/images/sprites/tart.png');
+const glogiTexture = PIXI.Texture.fromImage('./assets/images/sprites/glogi.png');
+const ballTexture = PIXI.Texture.fromImage('./assets/images/sprites/ball.png');
+
+const bouncers = [];
+
+let initialScale = 0.5;
+if ($(window).width() < 800){
+  initialScale = 0.25;
+};
+
+// Create beers
+for(let i = 0; i < 4; i++){
+  const beer = new PIXI.Sprite(beerTexture);
+  beer.x = Math.floor((Math.random() * window.innerWidth) + 1);
+  beer.y = Math.floor((Math.random() * window.innerHeight) + 1);
+  beer.anchor.x = 0.5;
+  beer.anchor.y = 0.5;
+  beer.scale = {x: initialScale, y:initialScale};
+  beer.speed = Math.floor((Math.random() * 5) + 1);
+  beer.rotationSpeed = (Math.random() * 0.05) + 0.01;
+  beer.speedX = beer.speed;
+  beer.speedY = beer.speed;
+  stage.addChild(beer);
+  bouncers.push(beer);
+}
+
+// Create tarts
+for(let i = 0; i < 3; i++){
+  const tart = new PIXI.Sprite(tartTexture);
+  tart.x = Math.floor((Math.random() * window.innerWidth) + 1);
+  tart.y = Math.floor((Math.random() * window.innerHeight) + 1);
+  tart.anchor.x = 0.5;
+  tart.anchor.y = 0.5;
+  tart.scale = {x: initialScale, y:initialScale};
+  tart.speed = Math.floor((Math.random() * 5) + 1);
+  tart.rotationSpeed = (Math.random() * 0.05) + 0.01;
+  tart.speedX = tart.speed;
+  tart.speedY = tart.speed;
+  stage.addChild(tart);
+  bouncers.push(tart);
+}
+
+// Create glogi
+for(let i = 0; i < 2; i++){
+  const glogi = new PIXI.Sprite(glogiTexture);
+  glogi.x = Math.floor((Math.random() * window.innerWidth) + 1);
+  glogi.y = Math.floor((Math.random() * window.innerHeight) + 1);
+  glogi.anchor.x = 0.5;
+  glogi.anchor.y = 0.5;
+  glogi.scale = {x: initialScale, y:initialScale};
+  glogi.speed = Math.floor((Math.random() * 5) + 1);
+  glogi.rotationSpeed = (Math.random() * 0.05) + 0.01;
+  glogi.speedX = glogi.speed;
+  glogi.speedY = glogi.speed;
+  stage.addChild(glogi);
+  bouncers.push(glogi);
+}
+
+// Create balls
+for(let i = 0; i < 3; i++){
+  const ball = new PIXI.Sprite(ballTexture);
+  ball.x = Math.floor((Math.random() * window.innerWidth) + 1);
+  ball.y = Math.floor((Math.random() * window.innerHeight) + 1);
+  ball.anchor.x = 0.5;
+  ball.anchor.y = 0.5;
+  ball.scale = {x: initialScale / 2, y:initialScale / 2}; // Why /2? Because balls are too big
+  ball.speed = Math.floor((Math.random() * 5) + 1);
+  ball.rotationSpeed = (Math.random() * 0.05) + 0.01;
+  ball.speedX = ball.speed;
+  ball.speedY = ball.speed;
+  stage.addChild(ball);
+  bouncers.push(ball);
+}
+
+
+// start animating
+animate();
+function animate() {
+    for (let i = 0; i < bouncers.length; i++){
+      let bounce = bouncers[i];
+      bounce.rotation += bounce.rotationSpeed;
+      bounce.position.x += bounce.speedX;
+      bounce.position.y += bounce.speedY;
+
+      if (bounce.position.x > window.innerWidth){
+        bounce.speedX = -bounce.speed;
+      } else if(bounce.position.x < 0) {
+        bounce.speedX = +bounce.speed;
+      }
+
+      if (bounce.position.y > window.innerHeight){
+        bounce.speedY = -bounce.speed;
+      } else if(bounce.position.y < 0) {
+        bounce.speedY = +bounce.speed;
+      }
+    }
+    requestAnimationFrame(animate);
+
+
+    // render the container
+    renderer.render(stage);
+}
+
+function randomDirection(){
+  const n = Math.floor((Math.random() * 2) + 1);
+  if( n === 1){
+    return 1;
+  } else {
+     return -1;
+  }
+}
